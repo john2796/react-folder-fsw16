2740b196298c7fe8e9f56616bd058db22fdf63b3
john2796 <jbmiranda22796@gmail.com>
2019-01-08 10:29:15 -0800

    student profile task

--- day-one/src/App.js
+++ day-one/src/App.js
@@ -1,17 +1,55 @@
 import React from "react";
 import "./App.css";
 
-import test from "../src/components"
-
-const Header = () => <h1>this is a header</h1>;
-const SubHeader = () => <p>this is a sub header</p>;
-
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <SubHeader />
-    </div>
-  );
+const HarryProfile = {
+  name: "Harry Potter",
+  identity: "The Chosen One",
+  age: 11,
+  bestFriend: "Ron Weasley and Hermione Granger"
 };
+
+const RonProfile = {
+  name: "Ron Weasley",
+  identity: "Youngest Weasley brother, Harry's best friend",
+  age: 11,
+  bestFriend: "Harry Potter and Hermione Granger"
+};
+
+const HermioneProfile = {
+  name: "Hermione Granger",
+  identity: "Smarty McSmart pants, Harry's best friend",
+  age: 11,
+  bestFriend: "Harry Potter and Ron Weasley"
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <StudentProfile student={HarryProfile} />
+        <StudentProfile student={RonProfile} />
+        <StudentProfile student={HermioneProfile} />
+      </div>
+    );
+  }
+}
+
+const StudentProfile = props => (
+  <div
+    style={{
+      margin: 10,
+      border: "1px solid gray",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      flexDirection: "column"
+    }}
+  >
+    <h1> {props.student.name}</h1>
+    <p> {props.student.identity}</p>
+    <p> {props.student.age}</p>
+    <p> {props.student.bestFriend}</p>
+  </div>
+);
+
 export default App;
